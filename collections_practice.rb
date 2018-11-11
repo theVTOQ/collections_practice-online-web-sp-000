ced2452daaa72134c660e4f7b56c2ed2ea65b9fd
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-11 20:17:58 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,8 +17,9 @@ end
 
 def swap_elements_from_to(array, index, destination_index)
   swapped_array = array
+  original = array[index]
   swapped_array[index] = array[destination_index]
-  swapped_array[destination_index] = array[index]
+  swapped_array[destination_index] = original
   swapped_array
 end
 
