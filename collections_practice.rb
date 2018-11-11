9f4c851132446f75836217f6bc258046b608dce5
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-11 19:54:13 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,3 +9,14 @@ end
 
 def sort_array_char_count(array)
   array.sort {|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+   swap_elements_from_to(array, 1, 2)
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  original = array[index]
+  array[index] = array[destination_index]
+  array[destination_index] = original
+end
